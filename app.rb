9c67684707c0711bdc11bf0dfea9c53b78765df9
Eilw <eirikwiig@gmail.com>
2016-02-02 14:04:13 +0000

    finished 19

--- app.rb
+++ app.rb
@@ -4,15 +4,22 @@ class Battle < Sinatra::Base
   # get '/' do
   #   'Hello Battle!'
   # end
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/names' do
-    @name = params[:Player1_name]
-    @name2 = params[:Player2_name]
+  get '/play' do
+    @name = session[:name]
+    @name2 = session[:name2]
     erb(:play)
+  end
+
+  post '/names' do
+    session[:name] = params[:Player1_name]
+    session[:name2] = params[:Player2_name]
+    redirect to('/play')
   end
 
   run! if app_file == $0
